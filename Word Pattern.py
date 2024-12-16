3de00b588c79ef649ef79dd7cdc697d1fca20519
Pranav <122866489+pranav-P92@users.noreply.github.com>
2024-12-16 22:01:27 +0530

    18th Dec

---
+++ Word Pattern.py
@@ -0,0 +1,24 @@
+#https://leetcode.com/problems/word-pattern/
+
+
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        counter={}
+        patternSignature=[]
+        for ind, char in enumerate(pattern):
+            if char not in counter:
+                counter[char]=ind
+            patternSignature.append(counter[char])
+
+        counter2={}
+        sSignature=[]
+        for ind,word in enumerate(s.split(" ")):
+            if word not in counter2:
+                counter2[word]=ind
+            sSignature.append(counter2[word])
+
+
+        return patternSignature==sSignature
+
+
