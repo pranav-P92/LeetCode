a75d3549dfa15edcda753306e97becf2cb5929ed
Pranav <122866489+pranav-P92@users.noreply.github.com>
2024-12-17 20:31:06 +0530

    16th Dec

--- Word Pattern.py
+++ Word Pattern.py
@@ -5,13 +5,13 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         counter={}
-        patternSignature=[]
+        patternSignature=[]   #pattern
         for ind, char in enumerate(pattern):
             if char not in counter:
                 counter[char]=ind
             patternSignature.append(counter[char])
 
-        counter2={}
+        counter2={}  
         sSignature=[]
         for ind,word in enumerate(s.split(" ")):
             if word not in counter2:
