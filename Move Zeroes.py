5137a0cab544296e6de51e1fe98d1322a7ceaacb
Pranav <122866489+pranav-P92@users.noreply.github.com>
2024-12-18 22:50:55 +0530

    18th Dec

--- Move Zeroes.py
+++ Move Zeroes.py
@@ -5,7 +5,7 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
-        Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in place instead.
         """
         l=0
         for i in range(len(nums)):
