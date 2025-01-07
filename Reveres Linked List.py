1fdfd3f9a5278210e985d56fd705aeed9fc38aee
Pranav <122866489+pranav-P92@users.noreply.github.com>
2025-01-07 22:31:31 +0530

    7th Jan

--- Reveres Linked List.py
+++ Reveres Linked List.py
@@ -6,6 +6,9 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         cur=head
