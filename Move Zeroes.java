5b7efebd832f9af9eeddd110772dc9647f13358a
Pranav <122866489+pranav-P92@users.noreply.github.com>
2025-11-17 21:18:25 +0530

    17th Nov

---
+++ Move Zeroes.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/move-zeroes/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]!=0){
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+            }
+        }
+    }
+}
