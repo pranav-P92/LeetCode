b307c7f98c83510606ec97a65bd6115de56b69ac
Pranav <122866489+pranav-P92@users.noreply.github.com>
2025-11-25 23:55:55 +0530

    25th Nov

---
+++ 3Sum.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+//2-pointer approach
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+       int i=0;
+       int j=numbers.length-1;
+       while(i<j) {
+        int sum=numbers[i]+numbers[j];
+        if(sum<target){
+            i++;
+        }
+        else if(sum>target){
+            j--;
+        }
+        else{
+       return new int[]{i+1,j+1};
+        }
+       }
+       return new int[]{-1,-1};
+    }
+}
