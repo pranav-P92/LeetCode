f3c9a89cdd75cc9f5fc45b098a7be94c629166de
Pranav <122866489+pranav-P92@users.noreply.github.com>
2025-11-27 00:42:13 +0530

    27th Nov

---
+++ 832. Flipping an Image.java
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/flipping-an-image/description/
+
+
+
+//2-pointer approach
+//time complexity : O(n^2)
+//space complexity : O(1)
+
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+       //flip bits
+        for(int i=0;i<image.length;i++){
+            for(int j=0;j<image.length;j++){
+                image[i][j]^=1;
+            }
+        }
+
+        //inverting bits
+        for(int l=0;l<image.length;l++){
+            int left=0;
+            int right=image[l].length-1;
+
+            while(left<right){
+                int temp=image[l][left];
+                image[l][left]=image[l][right];
+                image[l][right]=temp;
+                left++;
+                right--;
+            }
+        }
+        return image;
+    }
+}
