2cbadfe0ea7789f326049b62854c66e683af1a41
Pranav <122866489+pranav-P92@users.noreply.github.com>
2025-12-02 12:16:32 +0530

    2nd Dec

---
+++ 219. Contains Duplicate II.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/contains-duplicate-ii/description/
+
+//sliding-window approach
+//time complexity : O(n)
+//space complexity :O(k)
+
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+       Set<Integer> set=new HashSet<>();
+
+       for(int i=0;i<nums.length;i++){
+        if(set.contains(nums[i])){
+            return true;
+        }
+        set.add(nums[i]);
+        if(set.size()>k){
+            set.remove(nums[i-k]);
+        }
+       }
+       return false;
+    }
+}
