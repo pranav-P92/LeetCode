c1208bc1dccaa9354091f51539346099cef44719
Pranav <122866489+pranav-P92@users.noreply.github.com>
2025-12-05 22:52:18 +0530

    5th Dec

--- 2460. Apply Operations to an Array.java
+++ 2460. Apply Operations to an Array.java
@@ -8,12 +8,14 @@ class Solution {
     public int[] applyOperations(int[] nums) {
         int n=nums.length;
         int arr[]=new int[n];
+        
       for(int i=0;i<nums.length-1;i++){
         if(nums[i]==nums[i+1]){
             nums[i]*=2;
             nums[i+1]=0;
         }
       }  
+        
       int index=0;
       for(int j=0;j<n;j++){
         if(nums[j]!=0){
