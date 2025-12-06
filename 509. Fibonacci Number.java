9e752e82d611c97fc0b9c59ad70ba44d717be219
Pranav <122866489+pranav-P92@users.noreply.github.com>
2025-12-06 20:14:34 +0530

    6th Dec

---
+++ 509. Fibonacci Number.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/fibonacci-number/description/
+
+//time complexity  : O(n)
+//space complexity : O(1)
+
+
+
+class Solution {
+    public int fib(int n) {
+        if(n==0) return 0;
+        if (n==1) return 1;
+
+        int a=0;
+        int b=1;
+        int sum=0;
+        for(int i=2;i<=n;i++){
+            sum=a+b;
+            a=b;
+            b=sum;
+        }
+        return sum;
+    }
+}
