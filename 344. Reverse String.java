36ac2db3a06e094aa6fc449f889c43e4a678740c
Pranav <122866489+pranav-P92@users.noreply.github.com>
2025-12-07 23:01:32 +0530

    7th Dec

--- 344. Reverse String.java
+++ 344. Reverse String.java
@@ -1,7 +1,10 @@
 //https://leetcode.com/problems/reverse-string/description/
 
+
 //2-pointer approach
 
+//time complexity : O(n)
+//space complexity : 0(1)
 class Solution {
     public void reverseString(char[] s) {
          int i=0;
