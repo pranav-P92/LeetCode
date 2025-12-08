64d144c86e49beb1e31c91a53690ca19cf64161c
Pranav <122866489+pranav-P92@users.noreply.github.com>
2025-12-08 22:55:29 +0530

    8th Dec

---
+++ 2413. Smallest Even Multiple.java
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/smallest-even-multiple/description/
+
+
+
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2==0){
+            return n;
+        }
+        else{
+            return 2*n;
+        }
+    }
+}
