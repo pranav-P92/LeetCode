68b6f8eaa7e9b01b1b961fa67dc5c76f94fdd30f
Pranav <122866489+pranav-P92@users.noreply.github.com>
2026-01-05 22:46:36 +0530

    5th Jan

---
+++ 496. Next Greater Element I.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/next-greater-element-i/description/
+
+
+//stack
+//Time complexity  : O(n+m)
+//Space complexity : O(m)
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> map=new HashMap<>();
+        Stack<Integer> stack=new Stack<>();
+
+        for(int num:nums2){
+            while(!stack.isEmpty() && stack.peek()<num){
+                map.put(stack.pop(),num);
+            }
+            stack.push(num);
+        }
+
+        int res[]=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            res[i]=map.getOrDefault(nums1[i],-1);
+        }
+
+        return res;
+    }
+}
